a0e92ffacc5ecda4d35c478b2bdde9ad1dfa5fc8
aquabuz <aquabuz@gmail.com>
2023-11-07 21:59:28 +0900

    supabase install

--- env.d.ts
+++ env.d.ts
@@ -1,6 +1,8 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      VITE_SUPABASE_URL: string;
+      VITE_SUPABASE_ANON_KEY: string;
       VITE_APP_API_KEY: string;
       VITE_APP_ENV: string;
       // 다른 환경 변수도 필요한 경우 여기에 추가
